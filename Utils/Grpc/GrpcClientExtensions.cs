5656b2cf7cc742ea822482ec28d006a53000e58d
mondblad <mondblad@gmail.com>
2026-01-30 01:58:58 +0300

    Централизирование Grpc клиентов

---
+++ Utils/Grpc/GrpcClientExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Tutorium.Shared.Utils.Grpc
+{
+    public static class GrpcClientExtensions
+    {
+        public static void RegisterGrpcClient<TClient>(this WebApplicationBuilder builder, string serviceName) where TClient : class
+        {
+            builder.Services.Configure<GrpcServiceOptions>(serviceName, builder.Configuration.GetSection($"GrpcServices:{serviceName}"));
+            builder.Services.RegisterGrpcClient<TClient>(serviceName);
+        }
+
+        private static void RegisterGrpcClient<TClient>(this IServiceCollection services, string serviceName) where TClient : class
+        {
+            services.AddGrpcClient<TClient>((sp, options) =>
+            {
+                var cfg = sp
+                    .GetRequiredService<IOptionsMonitor<GrpcServiceOptions>>()
+                    .Get(serviceName);
+
+                options.Address = new Uri(cfg.Address);
+            });
+        }
+    }
+}
---
+++ Utils/Grpc/GrpcServiceOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Tutorium.Shared.Utils.Grpc
+{
+    public class GrpcServiceOptions
+    {
+        public string Address { get; set; } = null!;
+    }
+}
